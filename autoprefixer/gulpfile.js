1d7faadcc68b38b1eca3c0693ad84834d74642e6
Kyle Hunter <kyle.hunter91@gmail.com>
2020-06-25 07:16:08 -0700

    video 13 working with an auto prefixer

--- autoprefixer/gulpfile.js
+++ autoprefixer/gulpfile.js
@@ -1,14 +1,14 @@
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp = require("gulp");
+var autoprefixer = require("gulp-autoprefixer");
 
-
-gulp.task('styles',function() {
-  gulp.src('css/styles.css')
+gulp.task("styles", function (done) {
+  return gulp
+    .src("css/styles.css")
     .pipe(autoprefixer())
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest("build"));
+  // done();
 });
 
-
-gulp.task('watch',function() {
-  gulp.watch('css/styles.css', ['styles']);
+gulp.task("watch", function () {
+  gulp.watch("css/styles.css", ["styles"]);
 });
